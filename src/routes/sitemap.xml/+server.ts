38e48e36ac9c30a2280a1fd292fd43cc0868d372
luiscarrillo7 <archivo.luis.carrillo@gmail.com>
2026-03-26 19:39:51 -0500

    Corregida estructura de sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,3 +1,5 @@
+// src/routes/sitemap.xml/+server.ts
+
 export const GET = () => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
