5860e61c27553e89ba62e5eacafa07cf25cfd703
luiscarrillo7 <archivo.luis.carrillo@gmail.com>
2026-04-06 14:00:23 -0500

    Configurar adapter vercel

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -1,6 +1,6 @@
 <section class="relative min-h-[92vh] overflow-hidden text-white">
   <img
-    src="/ciudad-bicentenario.jpg"
+    src="/slogan.jpg"
     alt="Vista urbana de referencia"
     class="absolute inset-0 h-full w-full object-cover"
   />
